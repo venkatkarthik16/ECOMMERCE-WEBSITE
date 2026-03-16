9fec3df0117235e7fa8c74d4a62f41936ee0f915
venkatkarthik16 <venkat.karthik@cmr.edu.in>
2026-03-16 22:22:33 +0530

    moved orders page

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -2,6 +2,7 @@ import{Routes,Route} from 'react-router';
 
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';  
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Routes>  
       <Route index element={<HomePage/>}/>
       <Route path='checkout' element={<CheckoutPage/>}/>
+      <Route path='orders' element={<OrdersPage/>}/>
     </Routes>
   )
 }
