b12e911cc1b448edcbaced8dcbb3498b0b0c24ff
venkatkarthik16 <venkat.karthik@cmr.edu.in>
2026-03-18 20:26:45 +0530

    use axios to fetch data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,16 +1,15 @@
+ import axios from 'axios';
  import './HomePage.css';
+ 
  import { Header } from '../components/Header';
  import {products} from '../../starting-code/data/products';
  
  export function HomePage() {
-  fetch('http://localhost:3000/api/products')
+  axios.get('http://localhost:3000/api/products')
     .then((response) => {
-
-      return response.json();
-    })
-    .then((data) => {
-        console.log('Products from backend:', data);
-      });
+      console.log(response.data)
+    });
+    
 
   return (
     <>
