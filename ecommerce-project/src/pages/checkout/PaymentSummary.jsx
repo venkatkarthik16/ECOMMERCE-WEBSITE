4039d4f6f852ecf496466c6823ba11c67051dcf4
venkatkarthik16 <venkat.karthik@cmr.edu.in>
2026-03-23 09:33:01 +0530

    mke plce order button work

--- ecommerce-project/src/pages/checkout/PaymentSummary.jsx
+++ ecommerce-project/src/pages/checkout/PaymentSummary.jsx
@@ -1,6 +1,15 @@
 import { formatMoney } from "../../utils/money";
+import axios from "axios";
+import { useNavigate } from "react-router";
+export function PaymentSummary({ paymentSummary, loadCart }) {
+  const navigate = useNavigate();
 
-export function PaymentSummary({ paymentSummary }) {
+  const createOrder = async () => {
+    await axios.post("/api/orders");
+    await loadCart();
+    navigate("/orders");
+  };
+
   return (
     <div className="payment-summary">
       <div className="payment-summary-title">Payment Summary</div>
@@ -42,7 +51,10 @@ export function PaymentSummary({ paymentSummary }) {
             </div>
           </div>
 
-          <button className="place-order-button button-primary">
+          <button
+            className="place-order-button button-primary"
+            onClick={createOrder}
+          >
             Place your order
           </button>
         </>
